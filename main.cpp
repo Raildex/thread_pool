4b220534814144732bfcd8c6689d2b71037a94df
Nils <nils.gaitzsch@googlemail.com>
2022-04-19 20:18:32 +0200

    Adjusted Example

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@ unsigned int fibonacci(unsigned int num) {
 	return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-int main() {
+
+void parallel() {
 	std::chrono::nanoseconds concurrent = std::chrono::nanoseconds(0);
-	std::chrono::nanoseconds serial = std::chrono::nanoseconds(0);
 	for (int i = 0; i < 300; i++) {
 		auto start = std::chrono::high_resolution_clock::now();
 		{
@@ -23,28 +23,43 @@ int main() {
 			start = std::chrono::high_resolution_clock::now();
 			pool.enqueue([]() {
 				auto val = fibonacci(32);
-			});
+				});
 			pool.enqueue([]() {
 				auto val = fibonacci(32);
-			});
+				});
 			pool.enqueue([]() {
 				auto val = fibonacci(32);
-			});
+				});
 			pool.enqueue([]() {
 				auto val = fibonacci(32);
-			});
+				});
 		}
 		auto end = std::chrono::high_resolution_clock::now();
 		concurrent += end - start;
-		start = std::chrono::high_resolution_clock::now();
+	}
+	std::wcout << L"Parallel : Ø\t" << concurrent.count() / 300 << L"ns" << std::endl;
+}
+
+
+void serial() {
+	std::chrono::nanoseconds serial = std::chrono::nanoseconds(0);
+
+	for (int i = 0; i < 300; i++) {
+		auto start = std::chrono::high_resolution_clock::now();
+
 		auto val = fibonacci(32);
 		val = fibonacci(32);
 		val = fibonacci(32);
 		val = fibonacci(32);
-		end = std::chrono::high_resolution_clock::now();
+		auto end = std::chrono::high_resolution_clock::now();
 		serial += end - start;
-	}
 
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(concurrent).count() / 100 << std::endl;
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(serial).count() / 100 << std::endl;
+	}
+	std::wcout << L"Serial : Ø\t" << serial.count() / 300 << L"ns" << std::endl;
+
+}
+
+int main() {
+	serial();
+	parallel();
 }
